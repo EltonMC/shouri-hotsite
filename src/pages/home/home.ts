187ca8dcad7754bacd27b7d68d0dc2c62bfd49da
Elton <eltonmc@dcomp.ufs.br>
2018-05-26 12:25:24 -0300

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private iab: InAppBrowser){
+  }
+
+  showURL(url: string){
+    let browser = this.iab.create(url);
+    browser.show();
+  }
+
+}
